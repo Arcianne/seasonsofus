a6d94d89855b8ae29507a41ec2bb02fadb96be99
Arcianne <140175810+Arcianne@users.noreply.github.com>
2025-09-29 08:12:12 +0800

    create: landing page

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const images = document.querySelectorAll('.zoom-fade');
+
+    images.forEach((image) => {
+        image.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            const nextPageLink = image.parentElement.href;
+            image.classList.add('zoomed');
+
+            document.body.classList.add('fade-out');
+            setTimeout(() => {
+                window.location.href = nextPageLink;
+            }, 1000);
+        });
+    });
+
+    const audio = document.getElementById("bg-audio");
+    const muteBtn = document.getElementById("mute-btn");
+
+    audio.volume = 0.5; 
+
+    muteBtn.addEventListener("click", () => {
+    audio.muted = !audio.muted;
+    muteBtn.textContent = audio.muted ? "🔇" : "🔊";
+    });
